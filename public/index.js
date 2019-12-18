bbba61b0a82dbbf918a5c91f94e7d94b34966708
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2019-12-18 11:46:02 -0500

    12/18

--- public/index.js
+++ public/index.js
@@ -7,26 +7,52 @@
 
 const path = require('path');
 const express = require('express');  //express is a function that exist already
-
+const hbs = require("hbs");
 const app = express(); //app is a method that uses express. express().get,listern, etc turns into
 
 //definds paths for express
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views'); // views folder.
+const partialsPath = path.join(__dirname,'../templates/partials'); //added new partials folder so add new const
 
 // set up handlebars engine and views location
-app.set("views engine", "hbs"); // Telling express to use handlebars for templates
+app.set("view engine", "hbs"); // Telling express to use handlebars for templates
 app.set("views", viewsPath) // Telling express to get tempaths from the view folder 
+hbs.registerPartials(partialsPath); //hbs is using partials so set this up
 
 app.get("", async (req, res) => {
     try{
-        res.render("index");
-
+        res.render("index", {
+            title:
+        "Our first Express App"});
     } catch (error) {
-        res.status(500).send("page not found"/* This is what the error page displays*/);
+        res.status(500).send("page not found");
+    }
+});  
+
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id;
+    try{
+        res.render("index", {
+            title: `${name}`
+        });
+    } catch (error) {
+        res.status(500).send("page not found");
     }
 });
 
+app.get("/swag", async (req, res) => {
+    const name = req.params.id;
+    try{
+        res.render("swag");
+    } catch (error) {
+        res.status(500).send("page not found");
+    }
+}); 
+
+//http://127.0.0.1:3000/about/kaitongggao --> you get kaitonggao
+
+/* 
 app.get("", (req, res) => {
     res.send("Thami likes bagels and butter.")
 }); // "" = standard webpage and res.send sends a response
@@ -34,7 +60,7 @@ app.get("", (req, res) => {
 app.get("/kitty", (req, res) => {
     res.send("cats o:")
 }); 
-
+ */
 app.listen(3000, () => {
     console.log("Listening on port 3000");
     console.log(__dirname);
